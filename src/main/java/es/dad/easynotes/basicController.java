965c60d67461e3c0611eafb4798e8144c4dcd580
pavloXd <45689701+pavloXd@users.noreply.github.com>
2020-02-11 21:44:04 +0100

    Tablas Funcionando , sin datos

--- src/main/java/es/dad/easynotes/basicController.java
+++ src/main/java/es/dad/easynotes/basicController.java
@@ -6,6 +6,7 @@ import es.dad.easynotes.entity.Carrera;
 import es.dad.easynotes.entity.Universidad;
 import es.dad.easynotes.repository.ApunteRepository;
 import es.dad.easynotes.repository.AsignaturaRepository;
+import es.dad.easynotes.repository.CarreraRepository;
 import es.dad.easynotes.repository.UniversidadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,11 +26,17 @@ public class basicController {
 
     @Autowired
     private AsignaturaRepository asignaturaRepo;
+    
+    @Autowired
+    private CarreraRepository carreraRepo;
 
     @PostConstruct
     public void init() {
+    	/*
     	Carrera GIC = new Carrera("GIC");
+    	carreraRepo.save(GIC);
     	Carrera GII = new Carrera("GII");
+    	carreraRepo.save(GII);
         Universidad URJC = new Universidad("URJC", "Mostoles");
         universidadRepo.save(URJC);
         Universidad UPM = new Universidad("UPM", "Boadilla del Monte");
@@ -42,17 +49,18 @@ public class basicController {
         asignaturaRepo.save(ED);
         Asignatura logica = new Asignatura("Logica", URJC, GII, "Alexandra");
         asignaturaRepo.save(logica);
+        */
 
-        repositorioApunte.save(new Apunte(DAD, GIC, URJC,
-                /*Arrays.asList("Programacion", "Web", "Java")*/new ArrayList<>(), new File("/home/valen/latitude-e5440-laptop_owners-manual_en-us.pdf")));
-        repositorioApunte.save(new Apunte(POO, GII, URJC,
-                /*Arrays.asList("Programacion", "Java")*/new ArrayList<>(), new File("/home/valen/zswap.sh")));
-        repositorioApunte.save(new Apunte(ED, GIC, URJC,
-                /*Arrays.asList("Programacion", "C++")*/new ArrayList<>(), new File("/home/valen/zswap.sh")));
-        repositorioApunte.save(new Apunte(logica, GII, UPM,
-                /*Arrays.asList("Matematicas", "Primer orden")*/new ArrayList<>(), new File("/home/valen/arduinoSN.png")));
-        repositorioApunte.save(new Apunte(ED, GII, URJC,
-                new ArrayList<>(), new File("/home/valen/zswap.sh")));
+        //repositorioApunte.save(new Apunte(DAD, GIC, URJC,
+        //        /*Arrays.asList("Programacion", "Web", "Java")*/new ArrayList<>(), new File("/home/valen/latitude-e5440-laptop_owners-manual_en-us.pdf")));
+        //repositorioApunte.save(new Apunte(POO, GII, URJC,
+        //        /*Arrays.asList("Programacion", "Java")*/new ArrayList<>(), new File("/home/valen/zswap.sh")));
+        //repositorioApunte.save(new Apunte(ED, GIC, URJC,
+        //        /*Arrays.asList("Programacion", "C++")*/new ArrayList<>(), new File("/home/valen/zswap.sh")));
+        //repositorioApunte.save(new Apunte(logica, GII, UPM,
+        //        /*Arrays.asList("Matematicas", "Primer orden")*/new ArrayList<>(), new File("/home/valen/arduinoSN.png")));
+        //repositorioApunte.save(new Apunte(ED, GII, URJC,
+        //        new ArrayList<>(), new File("/home/valen/zswap.sh")));
     }
 
 }
