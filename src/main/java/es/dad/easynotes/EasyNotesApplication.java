7496e560f4437839b6bccd4fcf13a537a5e0f8aa
vlt23 <vlt23@protonmail.com>
2020-02-16 14:51:48 +0100

    Initial support for MySQL
    Copiar el fichero mysql.app.prop en application.properties cuando sustituyamos h2 por mysql

--- src/main/java/es/dad/easynotes/EasyNotesApplication.java
+++ src/main/java/es/dad/easynotes/EasyNotesApplication.java
@@ -3,8 +3,6 @@ package es.dad.easynotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import es.dad.easynotes.entity.Apunte;
-
 @SpringBootApplication
 public class EasyNotesApplication {
 
