80b47999c109bbfbd4ecb64cb723dae94cad001a
vlt23 <vlt23@protonmail.com>
2020-02-18 13:48:37 +0100

    Fix SQL sintax error debido a mayúsculas (aunque no debería...)

--- src/main/java/es/dad/easynotes/repository/ApunteRepository.java
+++ src/main/java/es/dad/easynotes/repository/ApunteRepository.java
@@ -1,14 +1,13 @@
   package es.dad.easynotes.repository;
 
-import java.util.List;
+  import es.dad.easynotes.entity.Apunte;
+  import es.dad.easynotes.entity.Asignatura;
+  import es.dad.easynotes.entity.Carrera;
+  import es.dad.easynotes.entity.Universidad;
+  import org.springframework.data.jpa.repository.JpaRepository;
+  import org.springframework.data.jpa.repository.Query;
 
-import es.dad.easynotes.entity.Asignatura;
-import es.dad.easynotes.entity.Carrera;
-import es.dad.easynotes.entity.Tag;
-import es.dad.easynotes.entity.Universidad;
-import org.springframework.data.jpa.repository.*;
-
-import es.dad.easynotes.entity.Apunte;
+  import java.util.List;
 
 public interface ApunteRepository extends JpaRepository<Apunte, Long> {
 
@@ -17,13 +16,9 @@ public interface ApunteRepository extends JpaRepository<Apunte, Long> {
 	List<Apunte> findByUniversidad(Universidad universidad);
 
 	List<Apunte> findByCarrera(Carrera carrera);
-	
-	List<Apunte> findByTags(Tag tag);
 
-	@Query(
-			value = "SELECT apunte.* FROM TAG JOIN APUNTE_TAGS on (tag.id = apunte_tags.tags_id) JOIN APUNTE on (apunte_tags.apuntes_id = apunte.id) WHERE (tag.tag =?1 )",
+	@Query(value = "SELECT apunte.* FROM tag JOIN apunte_tags on (tag.id = apunte_tags.tags_id) JOIN apunte on (apunte_tags.apuntes_id = apunte.id) WHERE (tag.tag =?1 )",
 			nativeQuery = true)
-			List<Apunte> findByTag(String tag);
-	
+	List<Apunte> findByTag(String tag);
 
 }
