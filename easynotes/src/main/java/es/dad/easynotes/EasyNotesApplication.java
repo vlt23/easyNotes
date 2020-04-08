ea755eebad45a47016b1a7c293783172e0dd14a0
pavloXd <45689701+pavloXd@users.noreply.github.com>
2020-04-08 23:15:43 +0200

    Modificado hazelcast, puesto como multicast. Añadido el docker-compose funcionando con hazelcast y bbdd

--- easynotes/src/main/java/es/dad/easynotes/EasyNotesApplication.java
+++ easynotes/src/main/java/es/dad/easynotes/EasyNotesApplication.java
@@ -2,6 +2,8 @@ package es.dad.easynotes;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,12 +19,13 @@ public class EasyNotesApplication {
 		SpringApplication.run(EasyNotesApplication.class, args);
 	}
 
+	@Value("${valor.hazelcast.ip}")
+	private String hostIp;
 	@Bean
 	public Config config() {
 		Config config = new Config();
 		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
-		joinConfig.getMulticastConfig().setEnabled(false);
-		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+		joinConfig.getMulticastConfig().setEnabled(true);
 		return config;
 	}
 
